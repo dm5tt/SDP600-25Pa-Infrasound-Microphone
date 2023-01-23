d9746050800d6001d7691f2c3a40d2625f09ba37
Holger Adams <mail@holad.de>
2023-01-23 16:15:51 +0100

    Move pictures to example directory

--- tools/create-spectrum.py
+++ tools/create-spectrum.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 
 
-data = np.genfromtxt('example.txt', dtype=np.int16)
+data = np.genfromtxt('data.txt', dtype=np.int16)
 
 f, t, Sxx = signal.spectrogram(data, 1 / 0.00459)
 
 fig, ax = plt.subplots()
-im = ax.imshow(Sxx, aspect='auto', origin='lower', extent=[t[0], t[-1], f[0], f[-1]], cmap='jet',  vmin=0, vmax=40)
+im = ax.imshow(Sxx, aspect='auto', origin='lower', extent=[t[0], t[-1], f[0], f[-1]], cmap='jet',  vmin=0, vmax=15)
 plt.colorbar(im)
 plt.xlabel('Time [s]')
 plt.ylabel('Frequency [Hz]')
